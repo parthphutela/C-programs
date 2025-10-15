38dbeda861de203745d64cecc725aaca9c59039d
parthphutela <parthphutela58@gmail.com>
2025-10-15 23:17:13 +0530

    Add files via upload

--- Experiment 4/exp4-1.c
+++ Experiment 4/exp4-1.c
@@ -1,1 +1,23 @@
-a
+    #include<stdio.h>
+    int globalvar = 10;
+    void func1()
+    {
+        printf("Inside fun2,globalvar modified to = %d\n",globalvar);
+    }
+    void func2() {
+        globalvar = 20;
+        printf("Inside fun2, globalvar modified to = %d\n, globalvar");
+    }
+    int main() {
+        printf("Inside main, globalvar = %d\n", globalvar);
+        func1();
+        func2();
+        printf("After func2 call, globalvar in main = %d\n", globalvar);
+    
+        return 0;
+    }
+
+    
+
+    
+
