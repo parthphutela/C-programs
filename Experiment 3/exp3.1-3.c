8e34e00c8cfcd2a6d5ed84f133020b76db9d86f3
parthphutela <parthphutela58@gmail.com>
2025-10-15 23:15:45 +0530

    Add files via upload

---
+++ Experiment 3/exp3.1-3.c
@@ -0,0 +1,26 @@
+//3 WAP to check if three points (x1,y1), (x2,y2) and (x3,y3) are collinear or not.
+
+#include <stdio.h>
+int main() {
+    float x1, y1, x2, y2, x3, y3;
+    float area;
+
+    printf("Enter coordinates of the first point (x1 y1): ");
+    scanf("%f %f", &x1, &y1);
+
+    printf("Enter coordinates of the second point (x2 y2): ");
+    scanf("%f %f", &x2, &y2);
+
+    printf("Enter coordinates of the third point (x3 y3): ");
+    scanf("%f %f", &x3, &y3);
+
+    area = 0.5 * (x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2));
+
+    if (area == 0) {
+        printf("The points are collinear.\n");
+    } else {
+        printf("The points are not collinear.\n");
+    }
+
+    return 0;
+}
