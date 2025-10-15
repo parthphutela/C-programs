7e9376cc24487ea1757770d1e95840b79fa57f66
parthphutela <parthphutela58@gmail.com>
2025-10-15 23:14:59 +0530

    Add files via upload

---
+++ Experiment 3/exp3.1-2.c
@@ -0,0 +1,31 @@
+//2 Write a program for BMI calculator
+
+#include <stdio.h>
+
+int main() {
+    float weight;
+    float height;
+    float bmi;
+
+    printf("Enter your weight in kilograms: ");
+    scanf("%f", &weight);
+
+    printf("Enter your height in meters: ");
+    scanf("%f", &height);
+
+    bmi = weight / (height * height);
+
+    printf("Your BMI is: %.2f\n", bmi);
+
+    if (bmi < 18.5) {
+        printf("You are underweight.\n");
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+        printf("You have a normal weight.\n");
+    } else if (bmi >= 25.0 && bmi < 29.9) {
+        printf("You are overweight.\n");
+    } else {
+        printf("You are obese.\n");
+    }
+
+    return 0;
+}
