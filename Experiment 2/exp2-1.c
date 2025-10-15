b9dc15431755a738a50c0270949cb4605625d0b0
parthphutela <parthphutela58@gmail.com>
2025-10-15 23:10:49 +0530

    Add files via upload

--- Experiment 2/exp2-1.c
+++ Experiment 2/exp2-1.c
@@ -1,1 +1,32 @@
-a
+//Experiment2
+//1 WAP a C program to calculate the area and perimeter of rectangle based on its length and width.
+
+#include <stdio.h>
+int main() {
+    printf(WAP a C program to calculate the area and perimeter of rectangle based on its length and width.\n");
+    int l ,b;
+    int p,a;
+    printf("Enter the length of the rectangle: ");
+    scanf("%d", &l);
+    printf("Enter the width of the rectangle: ");
+    scanf("%d", &b);
+    if (l<0 && b<0)
+    {
+    printf("Length and width can not be less than 0\n");
+    }
+    else if (b<0)
+    {
+    printf("Width can not be less than 0\n");
+    }
+    else if (l<0)
+    {
+    printf("Length can not be less than 0\n");
+    }
+    else
+    {
+    a=l*b
+    p=2*(l+b);
+    printf("Area of the rectangle: %d\n", a);
+    printf("Perimeter of the rectangle: %d\n", p);
+    return 0;
+}
