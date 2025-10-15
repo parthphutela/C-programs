9e97ea5203234cb1a5504ffa6f00fa1775f4938e
parthphutela <parthphutela58@gmail.com>
2025-10-15 23:08:18 +0530

    Add files via upload

---
+++ Experiment 1/exp1-2.c
@@ -0,0 +1,9 @@
+//2 Write a c program to print the address in multiple lines (new line)
+
+#include <stdio.h>
+int main()
+{
+    printf("Write a c program to print the address in multiple lines (new line)\n");
+    printf("365, \nSec-5A \nSaharanpur\n");
+    return 0;
+{}
